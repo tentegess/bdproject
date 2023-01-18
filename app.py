cb76070a33de18264383d9234b77bcb8b0c41ce2
tentegess <tenagent2222@gmail.com>
2023-01-18 02:39:51 +0100

    db connect works now

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import yaml
 app = Flask(__name__)
 
 #Configure db
-db = yaml.load(open('db.yaml'))
+db = yaml.safe_load(open('static/db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
@@ -17,7 +17,11 @@ mysql=MySQL(app)
 
 def index():
     cur = mysql.connection.cursor()
+    #cur.execute("INSERT INTO footballer(name, lastName) VALUES('Marcin','Najman')")
+    mysql.connection.commit()
+    cur.close()
     return render_template("index.html")
+
     
 
 #invalid URL
