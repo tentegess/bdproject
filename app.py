b4f4a6f00739bdd4ad8631fa19dfefff66de320e
kBaginskik <korniszonba@gmail.com>
2023-01-18 02:03:45 +0100

    db

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Admin123@localhost/grontball'
-app.config['SECRET_KEY'] = "Arek"
 
+#Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql=MySQL(app)
 
 @app.route('/')
 
 def index():
+    cur = mysql.connection.cursor()
     return render_template("index.html")
+    
 
 #invalid URL
 @app.errorhandler(404)
 
-#lol2
 def not_found(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
