19084563eb0ceae5d8bd5b4e1d218da436e2fa6b
tentegess <tenagent2222@gmail.com>
2023-01-24 03:12:10 +0100

    update footballer team and positions

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request, redirect, flash, ses
 from psycopg2 import sql
 import mysql.connector
 import yaml
-from datetime import date
+from datetime import date, datetime, timedelta
 
 
 app = Flask(__name__)
@@ -92,7 +92,7 @@ def list_of_ft():
     cur = mysql.cursor(dictionary=True)
     cur.execute("SELECT id, name FROM actions")
     action_list = cur.fetchall()
-    headings += list(map(lambda x: x['name'], action_list)) + ['Historia']
+    headings += list(map(lambda x: x['name'], action_list)) + ['Historia']+['Edycja']
     action_list_values = []
 
     for action in action_list:
@@ -112,6 +112,7 @@ def list_of_ft():
                     FROM positionhistory as ph 
                     JOIN position as p ON p.id = ph.id_position 
                     WHERE ph.id_footballer = f.id
+                    AND dateEnd is NULL
                     ), 'Brak pozycji') as position,
                 COALESCE(t.name, 'Brak druzyny') as team 
                 FROM footballer as f 
@@ -134,6 +135,10 @@ def footballerHistory(footballer_id):
     cur = mysql.cursor(dictionary=True)
     cur.execute("SELECT CONCAT(f.name, ' ', f.lastName) as last_name FROM footballer as f WHERE f.id = %s", (footballer_id,))    
     name = cur.fetchall()
+    if not name:
+        cur.close()
+        flash("Wybrany piłkarz nie istnieje","alert alert-danger")
+        return redirect(url_for("list_of_ft"))
 
     cur.execute("""
                 SELECT t.name AS club_name, dateFrom
@@ -183,7 +188,57 @@ def footballerHistory(footballer_id):
 
 
 
-@app.route('/add_footballer', methods=["POST","GET"])
+@app.route('/update_footballer/<footballer_id>', methods=["POST","GET"])
+
+def update_ft(footballer_id=None):
+    cur = mysql.cursor(dictionary=True)
+    cur.execute("""SELECT f.id, f.name, f.lastName, MAX(ch.dateFrom) AS clubDate, MAX(ph.dateFrom) AS positionDate from footballer f
+                LEFT JOIN clubhistory ch on f.id=ch.id_footballer
+                LEFT JOIN positionhistory ph on f.id=ph.id_footballer
+                WHERE f.id=%s AND ph.dateEnd IS NULL
+                """,(footballer_id,))
+    footballer=cur.fetchone()
+    print(footballer)
+    if not footballer['id']:
+        cur.close()
+        flash("Wybrany piłkarz nie istnieje","alert alert-danger")
+        return redirect(url_for("list_of_ft"))
+    if footballer['clubDate']:
+        footballer['clubDate']=footballer['clubDate']+timedelta(days=1)
+    if footballer['positionDate']:
+        footballer['positionDate']=footballer['positionDate']+timedelta(days=1)
+    cur.execute("SELECT id, name FROM teams")
+    teams_list=cur.fetchall()
+    cur.execute("SELECT id, name FROM position")
+    positions_list=cur.fetchall()
+    cur.close()
+    
+    if request.method == "POST":
+        cur = mysql.cursor()
+        try:
+            if request.form.get('name') and request.form.get('lastname'):
+                cur.execute("UPDATE footballer SET name=%s, lastName=%s WHERE id=%s",(request.form.get('name'),request.form.get('lastname'), footballer_id))
+                if request.form.get('actual_team') and request.form.get('dateFrom_team'):
+                   cur.execute("INSERT INTO clubhistory(id_footballer, id_team, dateFrom) VALUES(%s,%s,%s)",(footballer_id, request.form.get('actual_team'),request.form.get('dateFrom_team')))
+                if request.form.getlist('positions[]') and request.form.get('date_position'):
+                   cur.execute("UPDATE positionhistory SET dateEnd=%s WHERE id_footballer=%s AND dateEnd IS NULL",(request.form.get('date_position'), footballer_id))
+                   for i in range(0, len(request.form.getlist('positions[]'))):
+                        cur.execute("INSERT INTO positionhistory(id_footballer, id_position, dateFrom) VALUES(%s,%s,%s)",(footballer_id,request.form.getlist('positions[]')[i],request.form.get('date_position'))) 
+                cur.close()
+                mysql.commit()
+                flash("Pomyślnie zaktualizowano piłkarza","alert alert-success")
+                return redirect(url_for("list_of_ft"))
+            else:
+                cur.close()
+                flash("Należy uzupełnić dane","alert alert-danger")
+                return redirect(url_for("update_ft",footballer_id=footballer_id))
+        except Exception:
+                mysql.rollback()
+                cur.close()
+                flash("Wystąpił błąd w bazie danych","alert alert-danger")
+                return redirect(url_for("list_of_ft"))
+    
+    return render_template("update_footballer.html",teams=teams_list, positions=positions_list, footballer=footballer)
 
 #invalid URL
 @app.errorhandler(404)
